d71c09c4355f82adb1b9c96c8aab275eb03c12d4
pavanbairu <pavan.bairu1@gmail.com>
2024-12-10 15:41:24 +0530

    modular structure

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+from typing import List
+
+HYPHEN_E_DOT = "-e ."
+def getrequirements(file_path: str) -> List[str]:
+    """
+        this function will return the list of packages to be installed
+    """
+
+    with open(file_path) as file_obj:
+        lines = file_obj.readlines()
+        lines = [line.replace("\n", "") for line in lines]
+
+        if HYPHEN_E_DOT in lines:
+            lines.remove(HYPHEN_E_DOT)
+    
+    print("*"*50)
+    print("pavan")
+    return lines
+
+setup(
+
+    name = "ML-Project",
+    version= "0.0.1",
+    description= "Fraud detection",
+    author= "Pavan",
+    author_email= "pavan",
+    packages= find_packages(),
+    install_requires=getrequirements('requirements.txt')
+)
---
+++ src/exception.py
@@ -0,0 +1,21 @@
+import sys, os
+import logging
+
+from src.logger import logging
+
+sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
+def error_message_details(error, error_details:sys):
+    _,_,exc_tb = error_details.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    line_no = exc_tb.tb_lineno
+
+    return f"The error has been occured in python scipt name: {file_name} , line number: {line_no} , error message: {error}"
+
+
+class FraudException(Exception):
+    def __init__(self, error_message, error_details:sys):
+        super().__init__(error_message)
+        self.error_message = error_message_details(error=error_message, error_details=error_details)
+
+    def __str__(self):
+        return self.error_message
